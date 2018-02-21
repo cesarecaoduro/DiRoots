503250a2e6546761fce09cbdc745422218635396
Cesare Caoduro <cesare.caoduro@gmail.com>
2018-02-21 21:00:12 +0100

    ...

--- DiRoots/Structural.cs
+++ DiRoots/Structural.cs
@@ -1,24 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autodesk.DesignScript.Runtime;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using RevitServices.Persistence;
-using DiRoots;
 using Revit.Elements;
-using DSCore;
-using System.Collections;
 using ads = Autodesk.DesignScript.Geometry;
-using Autodesk.DesignScript.Geometry;
-using DynamoServices;
 using Revit.Elements.Views;
 using Revit.GeometryConversion;
-using RevitServices.Persistence;
 using RevitServices.Transactions;
+using Autodesk.DesignScript.Geometry;
 
 namespace DiRoots.Structural
 {
@@ -78,8 +71,8 @@ namespace DiRoots.Structural
             Revit.Elements.Element rebarHookTypeAtStart = null;
             Revit.Elements.Element rebarHookTypeAtEnd = null;
             Revit.Elements.Element rebarShape = null;
-            string rebarHookOrientationAtStart = "";
-            string rebarHookOrientationAtEnd = "";
+            string rebarHookOrientationAtStart = "Left";
+            string rebarHookOrientationAtEnd = "Left";
             string rebarStyle = null;
             double rebarDiameter = 0;
 
@@ -124,8 +117,8 @@ namespace DiRoots.Structural
         /// <param name="BarPositionIndex"></param>
         /// <returns></returns>
         [IsVisibleInDynamoLibrary(true)]
-        [MultiReturn(new[] { "Curves", "PolyCurve" })]
-        public static Dictionary<string, object> GetCenterLineCurves(Revit.Elements.Element Rebar, bool AdjustForSelfIntersection = true,bool SuppressHooks = true, bool SuppressBendRadius = true, bool MultiPlanarOption = true, int BarPositionIndex = 0)
+        [MultiReturn(new[] {"PolyCurve" })]
+        public static Dictionary<string, object> GetCenterLineCurves(Revit.Elements.Element Rebar, bool AdjustForSelfIntersection = false ,bool SuppressHooks = true, bool SuppressBendRadius = true, bool MultiPlanarOption = true, int BarPositionIndex = 0)
         {
             string msg = "Executed";
             Rebar rebRevit = doc.GetElement(Rebar.UniqueId.ToString()) as Rebar;
@@ -151,7 +144,7 @@ namespace DiRoots.Structural
 
                 foreach(Autodesk.Revit.DB.Curve c in centerLineCurves)
                 {
-                    curves.Add(RevitToProtoCurve.ToProtoType(c));
+                    curves.Add(RevitToProtoCurve.ToProtoType(c, true));
                 }
                 polyCurve = PolyCurve.ByJoinedCurves(curves);
             }
@@ -162,8 +155,7 @@ namespace DiRoots.Structural
 
             return new Dictionary<string, object>
             {
-                { "Curves", curves },
-                {"PolyCurve", polyCurve },
+                { "PolyCurve", polyCurve },
                 { "Message", msg },
             };
         }
@@ -259,7 +251,7 @@ namespace DiRoots.Structural
         /// <param name="PolyCurves"></param>
         /// <returns></returns>
         [IsVisibleInDynamoLibrary(true)]
-        [MultiReturn(new[] { "Curves" })]
+        [MultiReturn(new[] { "Rebars" })]
         public static Dictionary<string, object> RebarByCurves(
             Revit.Elements.Element Host,
             string RebarStyle,
@@ -268,30 +260,40 @@ namespace DiRoots.Structural
             Revit.Elements.Element RebarHookTypeAtEnd,
             string RebarHookOrientationAtStart,
             string RebarHookOrientationAtEnd,
-            List<ads.PolyCurve> PolyCurves
+            List<ads.PolyCurve> PolyCurves,
+            bool IsRevitOwned = false
             )
         {
             string msg = "Executed";
+            List<Revit.Elements.Element> dynRebar = new List<Revit.Elements.Element>();
 
 
             ElementId hostId = new ElementId(Host.Id);
             Autodesk.Revit.DB.Element h = doc.GetElement(hostId);
 
             RebarBarType rebType = RebarBarType.InternalElement as RebarBarType;
-            RebarHookType startHook = RebarHookTypeAtStart.InternalElement as RebarHookType;
-            RebarHookType endHook = RebarHookTypeAtEnd.InternalElement as RebarHookType;
+
+            RebarHookType startHook = RebarHookTypeAtStart == null ? null : (RebarHookType)RebarHookTypeAtStart.InternalElement;
+            //RebarHookType startHook = RebarHookTypeAtStart.InternalElement as RebarHookType;
+            RebarHookType endHook = RebarHookTypeAtEnd == null ? null : (RebarHookType)RebarHookTypeAtEnd.InternalElement;
+            //RebarHookType endHook = RebarHookTypeAtEnd.InternalElement as RebarHookType;
             RebarHookOrientation rebHookOrStart, rebHookOrEnd;
             RebarStyle rebStyle;
+            Rebar reb = null;
 
             if (RebarStyle == "Standard") { rebStyle = Autodesk.Revit.DB.Structure.RebarStyle.Standard; } else { rebStyle = Autodesk.Revit.DB.Structure.RebarStyle.StirrupTie; } 
             List<List<Autodesk.Revit.DB.Curve>> revitCurves = new List<List<Autodesk.Revit.DB.Curve>>();
 
             if (RebarHookOrientationAtStart == "Left") { rebHookOrStart = RebarHookOrientation.Left; } else { rebHookOrStart = RebarHookOrientation.Right; }
             if (RebarHookOrientationAtEnd == "Left") { rebHookOrEnd = RebarHookOrientation.Left; } else { rebHookOrEnd = RebarHookOrientation.Right; }
+            
 
             try
             {
-     
+                
+                Transaction tx = new Transaction(doc);
+                TransactionManager.Instance.EnsureInTransaction(doc);
+
                 string str = "";
                 foreach (var p in PolyCurves)
                 {
@@ -301,39 +303,76 @@ namespace DiRoots.Structural
 
                     foreach (var c in curves)
                     {
-                        convCurves.Add(ProtoToRevitCurve.ToRevitType(c));
+                        convCurves.Add(ProtoToRevitCurve.ToRevitType(c, true));
+                        Autodesk.Revit.DB.Curve cc = ProtoToRevitCurve.ToRevitType(c, true);
                         str += c.ToString();
                     }
+                        
                     try
                     {
-                        using (Transaction tx = new Transaction(doc, "CreateRebar"))
-                        {
-                            tx.Start("CreateRebar");
-                            Rebar reb = Rebar.CreateFromCurves(
-                                doc,
-                                rebStyle,
-                                rebType,
-                                startHook,
-                                endHook,
-                                h,
-                                normal,
-                                convCurves,
-                                rebHookOrStart,
-                                rebHookOrEnd,
-                                true,
-                                false
-                                );
-                            tx.Commit();
-                            tx.Dispose();
-                        }
-
+                            reb = Rebar.CreateFromCurves(
+                            doc,
+                            rebStyle,
+                            rebType,
+                            startHook,
+                            endHook,
+                            h,
+                            normal,
+                            convCurves,
+                            rebHookOrStart,
+                            rebHookOrEnd,
+                            true,
+                            true
+                            );
                     }
                     catch (Exception ex)
                     {
                         msg = "Error 2: " + ex.Message;
                     }
+
+                    dynRebar.Add(reb.ToDSType(IsRevitOwned));
                     revitCurves.Add(convCurves);
                 }
+                TransactionManager.Instance.TransactionTaskDone();
+            }
+            catch (Exception ex)
+            {
+                msg = "Error: " + ex.Message;
+            }
+
+            ;
+
+            return new Dictionary<string, object>
+            {
+                { "Rebars", dynRebar },
+                { "Message", msg },
+            };
+        }
+
+    }
+
+    public static class Visibility
+    {
+        public static Document doc = DocumentManager.Instance.CurrentDBDocument;
+        public static UIApplication uiapp = DocumentManager.Instance.CurrentUIApplication;
+        public static UIDocument uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument;
+        public static Functions f = new Functions();
+
+        [IsVisibleInDynamoLibrary(true)]
+        [MultiReturn(new[] { "Message" })]
+        public static Dictionary<string, object> SetSolidInView(Revit.Elements.Views.View View, List<Revit.Elements.Element> Rebars, bool SetSolid = true)
+        {
+            string msg = "Executed";
+            try
+            {
+                Autodesk.Revit.DB.View3D view = doc.GetElement(View.InternalElement.Id) as Autodesk.Revit.DB.View3D;
+                TransactionManager.Instance.EnsureInTransaction(doc);
+                foreach(Revit.Elements.Element reb in Rebars)
+                {
+                   Rebar r = doc.GetElement(reb.InternalElement.Id) as Rebar;
+                    r.SetSolidInView(view, SetSolid);
+                }
+                TransactionManager.Instance.TransactionTaskDone();
             }
             catch (Exception ex)
             {
@@ -342,11 +381,38 @@ namespace DiRoots.Structural
 
             return new Dictionary<string, object>
             {
-                {"Curves", revitCurves },
+                { "Message", msg },
+            };
+        }
+
+        [IsVisibleInDynamoLibrary(true)]
+        [MultiReturn(new[] { "Message" })]
+        public static Dictionary<string, object> SetUnobscuredInView(Revit.Elements.Views.View View, List<Revit.Elements.Element> Rebars, bool SetUnobscured = true)
+        {
+            string msg = "Executed";
+            try
+            {
+                Autodesk.Revit.DB.View3D view = doc.GetElement(View.InternalElement.Id) as Autodesk.Revit.DB.View3D;
+                TransactionManager.Instance.EnsureInTransaction(doc);
+                foreach (Revit.Elements.Element reb in Rebars)
+                {
+                    Rebar r = doc.GetElement(reb.InternalElement.Id) as Rebar;
+                    r.SetUnobscuredInView(view, SetUnobscured);
+                }
+                TransactionManager.Instance.TransactionTaskDone();
+            }
+            catch (Exception ex)
+            {
+                msg = "Error: " + ex.Message;
+            }
+
+            return new Dictionary<string, object>
+            {
                 { "Message", msg },
             };
         }
 
     }
 }
+
 
